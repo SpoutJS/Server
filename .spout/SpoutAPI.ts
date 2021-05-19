d48ea8108fda872be40ae5ebc5a39c85077583b3
Corman <mrccay12@gmail.com>
2021-05-19 17:40:14 -0400

    SpoutAPI

---
+++ .spout/SpoutAPI.ts
@@ -0,0 +1,3 @@
+export * from './core/apis';
+export { default as Event } from './core/events';
+export { default as SpoutServer } from './core/server';
--- .spout/internal/SpoutAPI.ts
+++
@@ -1,3 +0,0 @@
-export * from '../core/apis';
-export { default as Event } from '../core/events';
-export { default as SpoutServer } from '../core/server';
