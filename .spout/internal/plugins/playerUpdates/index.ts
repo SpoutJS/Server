0723f1e4318f32f8576d48e54b8dffeac1b03887
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-05-21 17:32:20 -0500

    Todo: fix client crashing

--- .spout/internal/plugins/playerUpdates/index.ts
+++ .spout/internal/plugins/playerUpdates/index.ts
@@ -1,55 +1,74 @@
-import { ConnectEvent } from "../login";
-import { sendPacketInfo } from "../playerInfo";
-import { SpoutServer, Event } from "../SpoutAPI";
+import { ConnectEvent } from '../login';
+import { sendPacketInfo } from '../playerInfo';
+import { SpoutServer, Event } from '../SpoutAPI';
 
 export const main = (server: SpoutServer<any>) => {
-    server.on("connect", (event: ConnectEvent) => {
-        const { player } = event;
-        server.broadcast(`&8[&2+&8] &7${player.username}`);
-        for (const other of server.players) {
-            if (other.uuid == player.uuid) continue;
-            console.log(
-                `Creating entity of ${other.username} for ${player.username}`
-            );
-            sendPacketInfo(player);
-            sendPacketInfo(other);
-            player.sendPacket("named_entity_spawn", {
-                entityId: other.id,
-                playerUUID: other.uuid,
-                x: 1,
-                y: 61,
-                z: 0,
-                yaw: 0,
-                pitch: 0,
-            });
-            setTimeout(() => {
-                other.sendPacket("named_entity_spawn", {
-                    entityId: player.id,
-                    playerUUID: player.uuid,
-                    x: 1,
-                    y: 61,
-                    z: 0,
-                    yaw: 0,
-                    pitch: 0,
-                });
-            });
-        }
-    });
-    setInterval(() => {
-        for (const player of server.players) {
-            for (const other of server.players) {
-                if(player.uuid === other.uuid) continue;
-                if (!player.location) player.location = {x:0, y:61, z:0, yaw:0, pitch:0};
-                other.sendPacket("entity_teleport", {
-                    entityId: player.id,
-                    x: player.location.x,
-                    y: player.location.y,
-                    z: player.location.z,
-                    yaw: player.location.yaw,
-                    pitch: player.location.pitch,
-                    onGround: true
-                });
-            }
-        }
-    }, 10);
+	server.on('connect', (event: ConnectEvent) => {
+		const { player } = event;
+		server.broadcast(`&8[&2+&8] &7${player.username}`);
+		for (const other of server.players) {
+			if (other.uuid == player.uuid) continue;
+			console.log(
+				`Creating entity of ${other.username} for ${player.username}`
+			);
+			sendPacketInfo(player);
+			sendPacketInfo(other);
+			player.sendPacket('named_entity_spawn', {
+				entityId: other.id,
+				playerUUID: other.uuid,
+				x: 1,
+				y: 61,
+				z: 0,
+				yaw: 0,
+				pitch: 0,
+			});
+			setTimeout(() => {
+				other.sendPacket('named_entity_spawn', {
+					entityId: player.id,
+					playerUUID: player.uuid,
+					x: 1,
+					y: 61,
+					z: 0,
+					yaw: 0,
+					pitch: 0,
+				});
+			});
+		}
+
+        // BIG BOI ERROR: Crashes client from server 
+		player._client.on(
+			'position',
+			({ x, y, z, onGround, yaw, pitch } = {}) => {
+				for (const other of server.players) {
+					if (player.uuid === other.uuid) continue;
+					other.sendPacket('entity_teleport', {
+						entityId: player.id,
+						x: x,
+						y: y,
+						z: z,
+						yaw: yaw,
+						pitch: pitch,
+						onGround: onGround,
+					});
+				}
+			}
+		);
+        player._client.on(
+			'position_look',
+			({ x, y, z, onGround, yaw, pitch } = {}) => {
+				for (const other of server.players) {
+					if (player.uuid === other.uuid) continue;
+					other.sendPacket('entity_teleport', {
+						entityId: player.id,
+						x: x,
+						y: y,
+						z: z,
+						yaw: yaw,
+						pitch: pitch,
+						onGround: onGround,
+					});
+				}
+			}
+		);
+	});
 };
